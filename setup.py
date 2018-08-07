fc66cd9439688aa6df0cd0c14eefe721e993352c
Kartik Mohta <kartikmohta@gmail.com>
2018-08-07 18:16:29 -0400

    Minor cleanup

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
     packages=['ss_segmentation'],
-    scripts=['nodes/ss_inference'],
     package_dir={'': 'src'}
 )
 
